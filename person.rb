5caa14bb1fc2b1a73496234c2f92b911b811f1f5
wuyepabdul <wuyepabdul@gmail.com>
2021-12-15 21:51:28 +0100

    Fix linters

--- person.rb
+++ person.rb
@@ -9,7 +9,7 @@ class Person
     @name = name
     @age = age
     @parent_permission = parent_permission
-    @corrector = Corrector.new()
+    @corrector = Corrector.new
   end
 
   def validate_name
