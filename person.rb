0e3289668ea9602ae81bbdee931a25d998ccb2f0
wuyepabdul <wuyepabdul@gmail.com>
2021-12-15 08:46:55 +0100

    Fix linters

--- person.rb
+++ person.rb
@@ -1,6 +1,6 @@
 class Person
   attr_accessor :name, :age, :parent_permission
-  attr_reader :id,
+  attr_reader :id
 
   def initialize(age:, name: 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
