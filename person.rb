fba3c9f77b73d82ff931409246ca98cde4b299ad
wuyepabdul <wuyepabdul@gmail.com>
2021-12-15 01:41:29 +0100

    Fix typo

--- person.rb
+++ person.rb
@@ -3,7 +3,7 @@ class Person
   attr_reader :parent_permission
 
   def initialize(age:, name: 'Unknown', parent_permission: true)
-    @id = Random.rand(1..10000)
+    @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
