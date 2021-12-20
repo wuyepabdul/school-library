5be9a0be27a59aad48cea8d33ee7ef1f7d577d36
brianorora <brianorora@gmail.com>
2021-12-20 18:05:52 +0300

    Refactor `create_rental` method

--- app.rb
+++ app.rb
@@ -34,25 +34,28 @@ class App
 
   private
 
-  def create_rental
+  def rental_input
     puts 'Select a book from the following list by number'
     @book_storage.each_with_index { |book, i| puts "#{i}) #{book}" }
-
     book_i = gets.chomp.to_i
-
     puts
     puts 'Select a person from the following list by number (not ID)'
     @person_storage.each_with_index { |person, i| puts "#{i}) #{person}" }
-
     person_i = gets.chomp.to_i
-
     puts
     print 'Date: '
     date = gets.chomp
+    [date, person_i, book_i]
+  end
 
+  def add_rental(date, person_i, book_i)
     rental = Rental.new(date, @person_storage.get_person_at_index(person_i), @book_storage.get_book_at_index(book_i))
     @rentals.push(rental)
+  end
 
+  def create_rental()
+    results = rental_input
+    add_rental(results[0], results[1], results[2])
     puts 'Rental created successfullyy'
   end
 
