00d59e5373f1aced92169524b23336a018359702
brianorora <brianorora@gmail.com>
2021-12-23 10:30:40 +0300

    Add tests for `Classroom` class

---
+++ spec/classroom_spec.rb
@@ -0,0 +1,18 @@
+require_relative '../classroom'
+require_relative '../student'
+
+
+describe Classroom do
+  it 'should initialzie it\'s parameters correctly' do
+    c = Classroom.new('class 1')
+    expect(c.label).to eql 'class 1'
+  end
+  
+  it 'should correctly add a student' do
+    c = Classroom.new('class 1')
+    s = Student.new(age: 6, classroom: c, name: 'Alfred')
+    c.add_student(s)
+    expect(c.students.length).to eql 1
+    expect(s.classroom).to be c
+  end
+end
